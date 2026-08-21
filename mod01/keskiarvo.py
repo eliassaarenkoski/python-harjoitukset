95a9b6c2cfeba21db78c6f9441df80c5de163093
Elias Saarenkoski <eliassaarenkoski@gmail.com>
2026-08-22 00:33:23 +0300

    vienti

---
+++ mod01/keskiarvo.py
@@ -0,0 +1,7 @@
+luku1 = int(input("anna kokonaisluku yksi: "))
+luku2 = int(input("anna kokonaisluku kaksi: "))
+luku3 = int(input("anna kokonaisluku kolme: "))
+keskiarvo = (luku1 + luku2 + luku3) / 3 
+summa = (luku1 + luku2 + luku3)
+tulo = (luku1 * luku2 * luku3)
+print (f"lukujen keskiarvo on:{keskiarvo: 2.1f} summa on:{summa} ja tulo on:{tulo}")
