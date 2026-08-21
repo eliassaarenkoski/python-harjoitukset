e31144a365e253f84b96d22b7d0caf5794ec816f
Elias Saarenkoski <eliassaarenkoski@gmail.com>
2026-08-22 01:38:22 +0300

    vienti

---
+++ mod01/koodinarpoja.py
@@ -0,0 +1,6 @@
+import random
+koodi = random.randint (0, 9),random.randint (0, 9),random.randint (0, 9)
+print (f"{koodi}")
+
+koodi2 = random.randint (1, 6),random.randint (1, 6),random.randint (1, 6), random.randint (1, 6)
+print (f"{koodi2}")
