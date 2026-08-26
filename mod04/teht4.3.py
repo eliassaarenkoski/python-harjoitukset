f7803e1548b593e5b5c763493dfcdd40c7a0bbbd
Elias Saarenkoski <eliassaarenkoski@gmail.com>
2026-08-27 00:33:59 +0300

    muutos

--- mod04/teht4.3.py
+++ mod04/teht4.3.py
@@ -1,18 +1,20 @@
-sukupuoli = input("mikä on suokupuolesi. Ilmoita mies tai nainen:")   
-arvo = float(input(" ilmoita hemoglobiiniarvo g/l "))
-    
-if sukupuoli == "mies" and arvo <135 :
-    print("alahainen hemoglobiini")
-elif sukupuoli == "mies" and 135 <= arvo <= 195 :
-    print("normaali hemoglobiini")
-else : 
-    sukupuoli == "mies" and arvo >195 
-    print ("korkea hemoglobiini")
+#Kirjoita ohjelma, joka kysyy käyttäjän biologisen sukupuolen ja hemoglobiiniarvon (g/l). Ohjelma ilmoittaa, onko hemoglobiiniarvo alhainen, normaali vai korkea.
+#Naisen normaali hemoglobiiniarvo on välillä 117-175 g/l.
+#Miehen normaali hemoglobiiniarvo on välillä 134-195 g/l.
 
-if sukupuoli == "nainen" and arvo <117 :
-    print("alahainen hemoglobiini")
-elif sukupuoli == "nainen" and 117 <= arvo <= 175  :
-    print("normaali hemoglobiini")
-else : 
-    sukupuoli == "nainen" and arvo >175  
-    print ("korkea hemoglobiini")
+sukupuoli = input("kerro biologinen sukupuolesi: ")
+arvo = float(input("anna hemoglobiini arvosi: "))
+
+if sukupuoli == "mies" and arvo < 134:
+    print(f"hemoglobiiniarvo on alhainen")
+elif sukupuoli == "mies" and 134<= arvo <=195:
+    print(f"hemoglobiiniarvo on normaali")
+elif sukupuoli == "mies" and arvo > 195:
+    print(f"hemoglobiiniarvo on korkea")
+
+if sukupuoli == "nainen" and arvo < 117:
+    print(f"hemoglobiiniarvo on alhainen")
+elif sukupuoli == "nainen" and 117<= arvo <=175:
+    print(f"hemoglobiiniarvo on normaali")
+elif sukupuoli == "nainen" and arvo > 175:
+    print(f"hemoglobiiniarvo on korkea")
